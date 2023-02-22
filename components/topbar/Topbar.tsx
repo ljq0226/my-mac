cefa525e027c701dd43f0fb9cbdec675d72bcbf7
ljq0226 <1035821043@qq.com>
2023-02-22 14:15:54 +0800

    feat:add day.js

--- components/topbar/Topbar.tsx
+++ components/topbar/Topbar.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Apple, BatteryCharge, More, Search, SwitchButton, Wifi } from '@icon-park/react'
 import TopbarItem from './TopbarItem'
+import dayjs from '@/configs/day'
 import { themeStore } from '@/store'
-
 interface TopBarState {
   date: Date
   showControlCenter: boolean
@@ -12,7 +12,7 @@ interface TopBarState {
 
 const Topbar = () => {
   const dark = themeStore(state => state.dark)
-  const data = new Date()
+  const date = new Date()
   return (
     <div className={`w-full h-8 px-2 fixed top-0 hstack 
        text-sm text-white bg-gray-700/10 backdrop-blur-2xl shadow transition
@@ -21,13 +21,13 @@ const Topbar = () => {
         <Apple theme="filled" size="16" fill={dark ? '#000' : '#fff'} />
       </div>
       <div className="flex-1"></div>
-      <div className='flex items-center justify-end h-full space-x-2 basis-1/6'>
+      <div className='flex items-center justify-end h-full space-x-2 '>
         <TopbarItem Icon={<Wifi theme="outline" size="16" fill="#333" />}></TopbarItem>
         <TopbarItem Icon={<Search theme="outline" size="16" fill="#333" />}></TopbarItem>
         <TopbarItem Icon={<More theme="outline" size="16" fill="#333" />}></TopbarItem>
         <TopbarItem Icon={<BatteryCharge theme="outline" size="16" fill="#333" />}></TopbarItem>
         <TopbarItem Icon={<SwitchButton theme="outline" size="16" fill="#333" />}></TopbarItem>
-
+        <div>{dayjs(date).format('MMMD ddd  HH:mm')}</div>
       </div>
 
     </div>
